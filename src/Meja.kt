9e878c8e0b684c1b1a295439b6997f3ffe985e26
Gompze <yosafat.de.santa@gmail.com>
2025-09-21 16:37:39 +0700

    add: Setter Getter on class Meja and Kursi

--- src/Meja.kt
+++ src/Meja.kt
@@ -1,3 +1,7 @@
 class Meja {
-
+    val id: Int,
+    val panjang: Int,
+    val lebar: Int,
+    val tinggi: Int,
+    val kapasitas: Int
 }
--- src/Ruang.kt
+++ src/Ruang.kt
@@ -1,3 +1,7 @@
 fun main() {
-
+    val id: Int,
+    val panjang: Int,
+    val lebar: Int,
+    val tinggi: Int,
+    val tipe: String
 }
